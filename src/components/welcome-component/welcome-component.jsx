de5f932c4212e54f4f48e7784a4a50e52aace6ba
Andrew Persad <akpersad@gmail.com>
2020-02-18 20:44:08 -0500

    Added fetch logic to pull quote. Refreshes every two hours minimum

--- src/components/welcome-component/welcome-component.jsx
+++ src/components/welcome-component/welcome-component.jsx
@@ -1,19 +1,62 @@
 import React, { Component } from "react";
 
 class WelcomeComponent extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			quote: ""
+		};
+	}
+
+	componentDidMount() {
+		if (window.localStorage.getItem("quote")) {
+			const quoteTime = new Date(window.localStorage.getItem("quoteDate"));
+			const twoHours = 60 * 60 * 1000 * 2; /* ms */
+			if (new Date() - quoteTime < twoHours) {
+				this.setState({ quote: window.localStorage.getItem("quote") });
+			} else {
+				this.fetchQuote();
+			}
+		} else {
+			this.fetchQuote();
+		}
+	}
+
+	fetchQuote() {
+		const backupQuote = "Only those who dare to fail greatly can ever achieve greatly.";
+		const url = "https://quotes.rest/qod";
+		fetch(url)
+			.then(results => {
+				results
+					.json()
+					.then(jsonResults => {
+						this.setState({ quote: jsonResults.contents.quotes[0].quote });
+						window.localStorage.setItem({
+							quote: jsonResults.contents.quotes[0].quote,
+							quoteDate: new Date()
+						});
+					})
+					.catch(err => {
+						console.log(err);
+						this.setState({ quote: backupQuote });
+					});
+			})
+			.catch(err => {
+				console.log(err);
+				this.setState({ quote: backupQuote });
+			});
+	}
+
 	render() {
+		const { quote } = this.state;
 		return (
 			<div className="welcome-comp-container">
 				{/* Enter component html here */}
 				<div className="image-overlay w-100 h-100">
 					<div className="container-fluid">
 						<div className="align-absolute_center flex-column">
-							<h1 className="header-title animate-pop-in">
-								Your awesome landing page
-							</h1>
-							<h3 className="header-subtitle animate-pop-in">
-								A useful start for your projects
-							</h3>
+							<h1 className="header-title animate-pop-in text-center">{quote}</h1>
 							<div className="d-block">
 								<section className="header-down-arrow">
 									<svg
