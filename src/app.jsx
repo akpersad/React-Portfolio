7ab3ccbad42b9f65724c50af7b434e5f2ecad551
Andrew Persad <akpersad@gmail.com>
2020-02-18 22:53:23 -0500

    Added Sticky Nav code from Gulp Repo

--- src/app.jsx
+++ src/app.jsx
@@ -4,6 +4,7 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 
 import Header from "./components/header/header";
 import MainComp from "./components/welcome-component/welcome-component";
+import StickyNav from "./components/sticky-nav/sticky-nav";
 
 class App extends Component {
 	render() {
@@ -12,8 +13,13 @@ class App extends Component {
 				<div id="main-header">
 					<Header />
 				</div>
-				<div id="welcome-comp">
-					<MainComp />
+				<div className="body-component">
+					<div id="welcome-comp">
+						<MainComp />
+					</div>
+					<div id="sticky-nav">
+						<StickyNav />
+					</div>
 				</div>
 			</>
 		);
