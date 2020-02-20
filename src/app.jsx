6d59ee52dd9ca0dbe1cc5b5436e20e31a5ed065d
Andrew Persad <akpersad@gmail.com>
2020-02-19 20:58:25 -0500

    Added Projects menu code

--- src/app.jsx
+++ src/app.jsx
@@ -5,6 +5,7 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 import Header from "./components/header/header";
 import MainComp from "./components/welcome-component/welcome-component";
 import StickyNav from "./components/sticky-nav/sticky-nav";
+import Projects from "./components/projects/projects";
 
 class App extends Component {
 	render() {
@@ -23,7 +24,9 @@ class App extends Component {
 
 					<div className="sections">
 						<section className="section-parts" id="one">
-							Hello
+							<div id="projects">
+								<Projects />
+							</div>
 						</section>
 					</div>
 
