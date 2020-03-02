f0a209d2b035eb299feac48e7bd851f7780dd002
Andrew Persad <akpersad@gmail.com>
2020-03-02 01:26:17 -0500

    Added social links and updated colors

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,19 +1,59 @@
 import React, { Component } from "react";
+import { FaTwitter, FaLinkedinIn, FaSalesforce, FaGithub } from "react-icons/fa";
 
+import constants from "../../global/_constants";
 import SubmitForm from "../submit-form/submit-form";
 
 class Footer extends Component {
 	render() {
 		return (
-			<section className="section-parts">
+			<section className="section-parts" id="six">
 				<div className="d-flex flex-wrap">
 					<div className="left-item footer-item">Left Item</div>
 					<div className="right-item footer-item">
 						<SubmitForm />
 					</div>
 					<div className="bottom-item footer-item_bottom mt-5">
-						<div className="social-icons d-flex justify-content-center">
-							<div className="social-icons_container">Icons</div>
+						<div className="social-icons d-flex flex-row flex-wrap justify-content-center mb-4">
+							<a
+								href={constants.socialSites.twitter}
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<div className="social-icons_item mx-5">
+									<FaTwitter size="1.2em" />
+								</div>
+							</a>
+
+							<a
+								href={constants.socialSites.linkedin}
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<div className="social-icons_item mx-5">
+									<FaLinkedinIn size="1.2em" />
+								</div>
+							</a>
+
+							<a
+								href={constants.socialSites.trailhead}
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<div className="social-icons_item mx-5">
+									<FaSalesforce size="1.2em" />
+								</div>
+							</a>
+
+							<a
+								href={constants.socialSites.github}
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<div className="social-icons_item mx-5">
+									<FaGithub size="1.2em" />
+								</div>
+							</a>
 						</div>
 
 						<div className="made-with d-flex justify-content-center">
