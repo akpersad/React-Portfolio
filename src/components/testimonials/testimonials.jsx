b0de5adee214d41043e363e77f5ba30404cc8fcf
Andrew Persad <akpersad@gmail.com>
2020-03-05 23:57:20 -0500

    Added beginning testimonial code

--- src/components/testimonials/testimonials.jsx
+++ src/components/testimonials/testimonials.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
+import Bodhi from "../../images/bodhi.png";
 
 class Testimonials extends Component {
 	render() {
@@ -17,22 +18,26 @@ class Testimonials extends Component {
 				{/* eslint-disable-next-line react/jsx-props-no-spreading */}
 				<Slider {...settings}>
 					<div className="slick-slider_item">
-						<h3>1</h3>
+						<div className="testimonial-container">
+							<div className="d-flex flex-column justify-content-center align-item-center text-center">
+								<div className="flex-image">
+									<img src={Bodhi} alt="Bodhi" />
+									<div className="image-circle" />
+								</div>
+
+								<div className="flex-name my-4">Andrew</div>
+
+								<div className="flex-words">
+									Andrew is so awesomoe! Andrew is so awesomoe! Andrew is so
+									awesomoe! Andrew is so awesomoe! Andrew is so awesomoe! Andrew
+									is so awesomoe! Andrew is so awesomoe! Andrew is so awesomoe!
+									Andrew is so awesomoe!
+								</div>
+							</div>
+						</div>
 					</div>
 					<div className="slick-slider_item">
 						<h3>2</h3>
-					</div>
-					<div className="slick-slider_item">
-						<h3>3</h3>
-					</div>
-					<div className="slick-slider_item">
-						<h3>4</h3>
-					</div>
-					<div className="slick-slider_item">
-						<h3>5</h3>
-					</div>
-					<div className="slick-slider_item">
-						<h3>6</h3>
 					</div>
 				</Slider>
 			</div>
