e0927108f47f28d57cc8ba655ec0cfd341597d63
Andrew Persad <akpersad@gmail.com>
2020-03-08 21:08:35 -0400

    Added welcome comp styles

--- src/components/welcome-component/welcome-component.jsx
+++ src/components/welcome-component/welcome-component.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import BackgroundImage from "../../images/desktop.png";
+import BackgroundImage from "../../images/802.jpg";
 
 class WelcomeComponent extends Component {
 	componentDidMount() {
@@ -15,13 +15,17 @@ class WelcomeComponent extends Component {
 				<div className="image-overlay w-100 h-100">
 					<div className="container-fluid">
 						<div className="align-absolute_center flex-column">
-							<h1 className="header-title animate-pop-in text-center">Placeholder</h1>
+							<h1 className="header-title animate-pop-in text-center">
+								Andrew Persad
+							</h1>
+							<h2 className="header-subtitle animate-pop-in text-center">
+								Front-end Engineer
+							</h2>
 							<div className="d-block">
 								<section className="header-down-arrow">
 									<svg
 										viewBox="0 0 32 32"
 										className="icon icon-chevron-bottom"
-										// viewBox="0 0 32 32"
 										aria-hidden="true"
 									>
 										<path d="M16.003 18.626l7.081-7.081L25 13.46l-8.997 8.998-9.003-9 1.917-1.916z" />
